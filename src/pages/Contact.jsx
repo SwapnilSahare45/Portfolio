0a5dda6ad2f6656a131425f540ca493f7234ad0f
Swapnil Sahare <swapnilsahare45@gmail.com>
2025-08-11 17:10:14 +0530

    fix: update contact page ui

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -6,11 +6,9 @@ const Contact = () => {
     return (
         <div id="contact" className="min-h-screen flex flex-col items-center justify-center px-4 py-8">
             <h1 className="font-orbitron uppercase tracking-wider text-4xl font-semibold md:text-6xl mb-8 z-2">Contact Me</h1>
-
-            <div className="z-2 flex md:w-1/2">
-
+ 
                 <form
-                    className="bg-[#333] w-full flex flex-col gap-4 rounded-lg p-8 font-gruppo shadow-md shadow-amber-300 hover:shadow-lg hover:scale-102 transition-all duration-300"
+                    className="z-2 bg-[#333] flex flex-col gap-4 rounded-lg p-8 font-gruppo shadow-md shadow-amber-300 hover:shadow-lg hover:scale-102 transition-all duration-300 md:w-1/2"
                 >
                     <p className="text-gray-300">Let’s talk — fill out the form and I’ll get back to you.</p>
                     <input
@@ -38,7 +36,6 @@ const Contact = () => {
                 </form>
 
             </div>
-        </div>
     )
 }
 
