8367ac4c1f870e82de4f2e4ad7fd78aa440be4e8
Swapnil Sahare <swapnilsahare45@gmail.com>
2025-08-12 16:50:09 +0530

    feat: add social links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import { FaLinkedin, FaGithubSquare } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <footer>
-      <p className='z-2 py-3 bg-[#333] text-center text-sm font-orbitron tracking-widest'>❤️ Design and Develoed by Swapnil Sahare</p>
-    </footer>
-  )
-}
+    <footer className="bg-[#333] text-white py-2 px-12 flex items-center justify-between">
+      <p className="text-sm md:text-base font-orbitron tracking-widest">
+        Design and Developed by ❤️ <span className="font-semibold">Swapnil Sahare</span>
+      </p>
 
-export default Footer
+      <div className="flex gap-3">
+        <a
+          href="https://www.linkedin.com/in/swapnil-sahare-06a803318"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-amber-300 shadow-lg shadow-amber-300"
+        >
+          <FaLinkedin className="text-3xl" />
+        </a>
+
+        <a
+          href="https://github.com/SwapnilSahare45"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-amber-300 shadow-lg shadow-amber-300"
+        >
+          <FaGithubSquare className="text-3xl" />
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
